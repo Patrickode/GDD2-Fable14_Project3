2cfb13f2b9d7e92c8a8f34d7105652e38c0accb2
crlimacastro <crlimacastro@gmail.com>
2020-11-11 01:21:45 -0500

    Started working on Potion creation

--- Fable14_Project3/Assets/Scripts/ScriptableObjects/PotionType.cs
+++ Fable14_Project3/Assets/Scripts/ScriptableObjects/PotionType.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New PotionType", menuName = "ScriptableObjects/PotionType")]
 public class PotionType : ScriptableObject
 {
-    public List<Ingredient> ingredients;
+    // Exact attribute count for each attribute needed to make the potion
+    public Dictionary<IngredientAttribute, int> attributesRequired;
+
     public Color color;
 }
