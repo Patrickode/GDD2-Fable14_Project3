84e0e6c5d2fc8d2d2c636a04dd18ba51768a69ee
Patrick <prm5983@rit.edu>
2020-11-21 12:33:10 -0500

    Added code to differentiate ingredient pieces without an image

--- Fable14_Project3/Assets/Scripts/Managers/PieceManager.cs
+++ Fable14_Project3/Assets/Scripts/Managers/PieceManager.cs
@@ -62,6 +62,36 @@ public class PieceManager : MonoBehaviour
         {
             spawnedIng.sprite = correspondingImage;
         }
+        else
+        {
+            switch (ingToSpawn.SenseType)
+            {
+                default:
+                case IngredientType.LiquidBase:
+                    break;
+                case IngredientType.Sight:
+                    spawnedIng.color = Color.red;
+                    break;
+                case IngredientType.Sound:
+                    spawnedIng.color = new Color(1, 0.498f, 0, 1);
+                    break;
+                case IngredientType.Smell:
+                    spawnedIng.color = Color.yellow;
+                    break;
+                case IngredientType.Taste:
+                    spawnedIng.color = Color.green;
+                    break;
+                case IngredientType.Touch:
+                    spawnedIng.color = Color.blue;
+                    break;
+                case IngredientType.Mind:
+                    spawnedIng.color = new Color(0.624f, 0, 0.773f, 1);
+                    break;
+                case IngredientType.Soul:
+                    spawnedIng.color = new Color(1, 0.412f, 0.706f, 1);
+                    break;
+            }
+        }
 
         spawnedPieces.Add(spawnedIng);
     }
