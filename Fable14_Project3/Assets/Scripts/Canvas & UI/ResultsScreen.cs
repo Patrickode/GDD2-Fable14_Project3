228a8feb7082285df23d10235c6e0508fbde8e28
Patrick <prm5983@rit.edu>
2020-11-26 13:12:17 -0500

    Minor polish details
    - Time changes color when day is over
    - Tweaked new high score color
    - Added delay to results screen animation
    - Added cheat code to teleport to end of day

--- Fable14_Project3/Assets/Scripts/Canvas & UI/ResultsScreen.cs
+++ Fable14_Project3/Assets/Scripts/Canvas & UI/ResultsScreen.cs
@@ -10,6 +10,7 @@ public class ResultsScreen : MonoBehaviour
     [SerializeField] private TextMeshProUGUI highScoreLabel = null;
     [SerializeField] private ScoreManager scoreManager = null;
     [SerializeField] private Color newHighScoreColor = Color.yellow;
+    [SerializeField] private float inAnimDelay = 1;
 
     private void Start()
     {
@@ -26,8 +27,13 @@ public class ResultsScreen : MonoBehaviour
     {
         float cScore = scoreManager.CurrentScore;
         float hScore = scoreManager.HighScore;
-        currentScoreLabel.text = $"Today's Earnings: ${cScore:F2}";
-        highScoreLabel.text = $"Today's Earnings: ${hScore:F2}";
+
+        //If current score is negative, put the negative before the dollar sign.
+        //We don't need to do this for the high score, it will always be >= 0.
+        currentScoreLabel.text = cScore < 0
+            ? $"Today's Earnings: -${-cScore:F2}"
+            : $"Today's Earnings: ${cScore:F2}";
+        highScoreLabel.text = $"Record Earnings: ${hScore:F2}";
 
         if (cScore >= hScore)
         {
@@ -35,6 +41,13 @@ public class ResultsScreen : MonoBehaviour
             highScoreLabel.color = newHighScoreColor;
         }
 
+        StartCoroutine(DelayInAnim(inAnimDelay));
+        CustomerManager.LastCustomerDequeued -= ShowResultsScreen;
+    }
+
+    private IEnumerator DelayInAnim(float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
         gameObject.SetActive(true);
         resultsAnimator.SetTrigger("In");
     }
